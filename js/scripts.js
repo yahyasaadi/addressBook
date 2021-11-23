7cafeae767a39bf4c6b7046971cb91db4dedc138
yahyasaadi <yahyasaadi9219@gmail.com>
2021-11-23 10:44:05 +0300

    Added the fullName() prototype

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,13 @@ function Contact(first, last){
     this.lastName = last;
 }
 
+
+Contact.prototype.fullName = function(){
+    return this.firstName + " " + this.lastName;
+}
+
+
+
 // User Interface Logic
 $(document).ready(function(){
     $("form#new-contact").submit(function(event){
@@ -14,7 +21,7 @@ $(document).ready(function(){
 
         var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
-        $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+        $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
         $("input#new-first-name").val("");
         $("input#new-last-name").val("");
