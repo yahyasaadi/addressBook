bbd0753c7e4fdcd7b15626e3ccce79e4bfdc66a4
yahyasaadi <yahyasaadi9219@gmail.com>
2021-11-23 10:25:37 +0300

    Added a constructor to the js file

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+// Busines Logic
+function Contact(first, last){
+    this.firstName = first;
+    this.lastName = last;
+}
+
+// User Interface Logic
+$(document).ready(function(){
+    $("form#new-contact").submit(function(event){
+        event.preventDefault();
+
+        var inputtedFirstName = $("input#new-first-name").val();
+        var inputtedLastName = $("input#new-last-name").val();
+
+        var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+        $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+
+        $("input#new-first-name").val("");
+        $("input#new-last-name").val("");
+    });
+});
